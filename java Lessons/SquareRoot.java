8df13ec744822ff4758d8664166fd595ba6edc71
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-06-12 16:42:19 -0700

    Add files via upload

---
+++ java Lessons/SquareRoot.java
@@ -0,0 +1,21 @@
+public class SquareRoot {
+	public static void main(String[] args) {
+	
+		System.out.print("The square root of " + test(2));
+
+
+}
+
+
+	public static int test(int num) {
+
+		int result = num * 2;
+
+
+			return result;
+
+
+
+}
+
+}
