226447f4ab075b4643db1224e03776e98b0ee9da
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-11 00:58:43 +0100

    Add files via upload

---
+++ java Lessons/MyQuestionsAndAnswer/MY TASKS/MultiplesOf3.java
@@ -0,0 +1,9 @@
+public class MultiplesOf3 {
+    public static void main(String[] args) {
+        for (int count = 1; count <= 15; count++) {
+            if (count % 3 == 0) {
+                System.out.println(count);
+            }
+        }
+    }
+}
