29dc611925c5b132a44f0b632bf4478c7e957775
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-11 01:15:34 +0100

    Add files via upload

---
+++ java Lessons/MyQuestionsAndAnswer/MY TASKS/SquareNumbers.java
@@ -0,0 +1,8 @@
+public class SquareNumbers {
+    public static void main(String[] args) {
+        for (int count = 1; count <= 5; count++) {
+            int square = count * count;
+            System.out.println(count + " squared is " + square);
+        }
+    }
+}
