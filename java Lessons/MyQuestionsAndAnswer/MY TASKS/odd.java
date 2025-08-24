abb38720ad100f1b4b8e93c60f4cbc10d749303a
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-08-24 16:32:37 +0100

    Add files via upload

---
+++ java Lessons/MyQuestionsAndAnswer/MY TASKS/odd.java
@@ -0,0 +1,16 @@
+public class odd{
+	public static void oddNumber(int[] Array){
+	for(int count = 0; count < Array.length; count++){
+		if(Array[count] % 2 == 0){
+	System.out.print(Array[count] + " ");
+
+}
+}
+}
+public static void main(String[] args){
+int[] count = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+oddNumber(count);
+
+}
+
+}
