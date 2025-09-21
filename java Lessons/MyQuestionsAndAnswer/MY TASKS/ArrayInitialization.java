f1b4617f5d8e05858fb1d0261d8295c23f28f24e
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-09-21 22:55:08 +0100

    Add files via upload

---
+++ java Lessons/MyQuestionsAndAnswer/MY TASKS/ArrayInitialization.java
@@ -0,0 +1,17 @@
+public class ArrayInitialization {
+        public static void main(String[] args) {
+            int[][] matrix = {
+                    {1, 2},
+                    {3, 4}
+            };
+
+            for (int[] row : matrix) {
+                for (int val : row) {
+                    System.out.print(val + " ");
+                }
+                System.out.println();
+            }
+        }
+}
+
+
