44445d4e2b57283b44349f64db456fb242bf5893
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-09-26 21:54:46 +0100

    Add files via upload

---
+++ java Lessons/MyQuestionsAndAnswer/MY TASKS/GreetingFunction.java
@@ -0,0 +1,9 @@
+public class GreetingFunction {
+    public static void greet() {
+        System.out.println("Hello! Welcome to Java.");
+    }
+
+    public static void main(String[] args) {
+        greet();
+    }
+}
