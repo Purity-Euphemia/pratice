e7cf41b3cf5f7eff3538637598b6b9b301c9b9b7
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-09-26 21:56:33 +0100

    Add files via upload

---
+++ java Lessons/MyQuestionsAndAnswer/MY TASKS/SumFunction.java
@@ -0,0 +1,9 @@
+public class SumFunction {
+    public static void printSum(int a, int b) {
+        System.out.println("Sum: " + (a + b));
+    }
+
+    public static void main(String[] args) {
+        printSum(5, 10);
+    }
+}
