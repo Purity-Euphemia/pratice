fc1d6c177b37ea0c788f5487ebc0c68aef3ef4bd
Purity-Euphemia <euphemiaobachie@gmail.com>
2025-09-27 22:38:31 +0100

    Add files via upload

---
+++ java Lessons/MyQuestionsAndAnswer/MY TASKS/MaxFunction.java
@@ -0,0 +1,13 @@
+public class MaxFunction {
+
+        public static int max(int a, int b) {
+            return (a > b) ? a : b;
+        }
+
+        public static void main(String[] args) {
+            int result = max(8, 15);
+            System.out.println("Maximum: " + result);
+        }
+    }
+
+
